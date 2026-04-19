34fb8069a3b6a1766b84a4865fbe2234276b5fb5
Darun Karas Abir <darun15-14188@diu.edu.bd>
2026-04-19 12:49:04 +0600

    learn model

---
+++ Models/Book.cs
@@ -0,0 +1,9 @@
+namespace MyApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }          // book id
+        public string Title { get; set; }    // book name
+        public string Author { get; set; }   // author name
+    }
+}
