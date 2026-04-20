d674d74d6e59f84fc3ccdbee7e6346fbe32f3fb3
Darun Karas Abir <darun15-14188@diu.edu.bd>
2026-04-20 21:47:20 +0600

    mvc pattern

--- Models/Book.cs
+++
@@ -1,9 +0,0 @@
-namespace MyApp.Models
-{
-    public class Book
-    {
-        public int Id { get; set; }          // book id
-        public string Title { get; set; }    // book name
-        public string Author { get; set; }   // author name
-    }
-}
